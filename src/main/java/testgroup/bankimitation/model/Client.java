c1ad51409a8609d75904ac5de547eaa4f0a5b35f
TimurFarh <timurfar192@gmail.com>
2019-11-23 22:19:45 +0500

    Fix problem with 'Edit client'

--- src/main/java/testgroup/bankimitation/model/Client.java
+++ src/main/java/testgroup/bankimitation/model/Client.java
@@ -31,6 +31,10 @@ public class Client {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
     }
